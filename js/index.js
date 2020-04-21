7f9c434b4569748e39ba7709c40df644b4fe2549
Lais Monteiro <lmonteiro189@gmail.com>
2020-04-21 19:07:09 +0100

    Not solved in time

--- js/index.js
+++ js/index.js
@@ -3,18 +3,43 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  //set the price variable acessing the price cell > span > innerText
+  let $price = product.querySelector('.price span');
+  //change the string to a number
+  const priceValue = Number($price.innerText);
+  //set the quantity value to access the value inputed by the user
+  const $quantity = product.querySelector('.quantity input');
+  const quantityValue = $quantity.valueAsNumber;
+  //multuplying both to get a subtotal
+  const subtotal = priceValue * quantityValue;
+  //use the subtotal to change the visual subtotal
+  const $subtotalArea = product.querySelector('.subtotal span');
+  $subtotalArea.innerText = subtotal;
+
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
+  const allProducts = document.querySelector(".product");
+  console.log(allProducts);
+
+  updateSubtotal(allProduct);
+  const $subtotal = produtct.querySelector(".subtotal span");
+  const $subtotal = $subtotal.innerText;
+  const $total = product.querySelector("$total-value span");
+  $total.innerText = $subtotal;
 
   // ITERATION 2
   //... your code goes here
+  const inputPriceQuantity = function () {
+    
+    }
+
 
   // ITERATION 3
   //... your code goes here
